9482763a34261d45316d9916743b384f1ed51aad
Braiam Peguero <braiamp@gmail.com>
2013-03-06 15:13:48 -0400

    Adding icons to the popup
    Fixes #31
    Signed-off-by: Braiam Peguero <braiamp@gmail.com>

--- js/popup.js
+++ js/popup.js
@@ -2582,4 +2582,8 @@ $(function () {
   $("#butFind").click(function (e) {
     search();
   });
+  $('img').each( function() {
+    var o = $(this);
+    if( ! o.attr('title') && o.attr('alt') ) o.attr('title', o.attr('alt') );
+  });
 });
