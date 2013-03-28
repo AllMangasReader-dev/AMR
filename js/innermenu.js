405d7a4cd790addb3ce0621612c5aea39c772f03
polo <pl.duhoux@gmail.com>
2013-03-28 08:16:41 +0100

    Licensing AMR
    I added the licensing header on every file (except fas and dev and
    release which will be moved out of the app)
    @braiam : I used np++, effectively pspad is wrongly encoding in utf8...

--- js/innermenu.js
+++ js/innermenu.js
@@ -1,4 +1,23 @@
-﻿var menusAMR = [
+﻿/**
+
+  This file is part of All Mangas Reader.
+  
+  All Mangas Reader is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  All Mangas Reader is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with All Mangas Reader.  If not, see <http://www.gnu.org/licenses/>. 
+
+*/
+
+var menusAMR = [
   {name: "options", link: "options.html", title: "Options", icon: "options.png"},
   {name: "search", link: "search.html", title: "Search", icon: "find.png"},
   {name: "bookmarks", link: "bookmarks.html", title: "Bookmarks", icon: "bookmark.png"},
