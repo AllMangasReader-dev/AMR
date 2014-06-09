2f5aac78ad044ef41ae855865994ff6eb09c8219
Braiam Peguero <braiamp@gmail.com>
2014-06-08 22:46:16 -0400

    Disables new mirrors notifications, for the moment

--- js/mgEntry.js
+++ js/mgEntry.js
@@ -642,7 +642,7 @@ function waitForActivatedAndListFinish(mirrorsT, callback) {
 
 function displayNotification(wsData, params) {
   "use strict";
-  var text = "";
+/*  var text = "";
   if (wsData.revision > 0) {
     if (wsData.isnew) {
       text = "Implementation created by " + wsData.developer + ".";
@@ -668,5 +668,5 @@ function displayNotification(wsData, params) {
     setTimeout(function () {
       notif.cancel();
     }, params.notificationtimer * 1000);
-  }
+  }*/
 }
